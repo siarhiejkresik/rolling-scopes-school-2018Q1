0bad6cac78c8534af138e43d3d42fcfcb59ae62e
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-21 12:21:52 +0300

    Move Game class to a separate module

---
+++ match-match-game/scripts/Game.js
@@ -0,0 +1,44 @@
+import OpenedCardsQueue from "./CardsQueue.js";
+
+export default class Game {
+    constructor(cards, SEQUENCE_SIZE) {
+        this.cards = cards;
+        this.cardsToPlay = cards.length;
+        this.openedCards = new OpenedCardsQueue(SEQUENCE_SIZE);
+    }
+
+    isSelectableCard(card) {
+        return (card.isClosed && !this.openedCards.blocked);
+    }
+
+    getCardByIndex(cardIndex) {
+        return this.cards[cardIndex];
+    }
+
+    selectCard(cardIndex) {
+        const card = this.getCardByIndex(cardIndex);
+
+        if (card.id !== cardIndex) {
+            throw 'wrong cardId';
+        }
+
+        if (!this.isSelectableCard(card)) {
+            console.log('you can\'t select this card!', card);
+            return;
+        }
+
+        this.openedCards.add(card);
+        if (this.openedCards.process()) {
+            this.cardsToPlay -= this.openedCards.size;
+            this.isGameOver();
+        }
+    }
+
+    isGameOver() {
+        const result = this.cardsToPlay === 0 ? true : false;
+        if (result) {
+            console.log('You win!');
+        }
+        return result;
+    }
+}
