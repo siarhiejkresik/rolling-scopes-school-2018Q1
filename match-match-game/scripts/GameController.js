8453d66fad3a6a805e93c8c53127b8baf75d7dd7
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-22 23:41:14 +0300

    Implement GameController class

---
+++ match-match-game/scripts/GameController.js
@@ -0,0 +1,25 @@
+import GameView from "./GameView.js";
+import GameModel from "./GameModel.js";
+
+
+export default class GameController {
+    constructor(settings) {
+        this.view = new GameView(settings.cards);
+        this.view.addListener('click', this.processEvent, this);
+        this.model = new GameModel(settings, this.view);
+    }
+
+    processEvent(e) {
+        const cardIndex = e.target.id;
+        if (!e.target.closest('.card')) {
+            return;
+        }
+        let cardId = e.target.closest('.card').id;
+        cardId = cardId.split('-').slice(-1)[0];
+        cardId = parseInt(cardId, 10);
+        console.log('CLICKED id:', cardId, e.target);
+        this.model.selectCard(cardId);
+        console.log(this.model.cards.map(c => `${c._state} ${c.type}`));
+        console.log('-----------------')
+    }
+}
