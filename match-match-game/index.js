ab01d1ff270600412ee05baaafafcae9e6f2bf40
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-22 23:43:18 +0300

    Remove unused code in index.js, add settings object

--- match-match-game/index.js
+++ match-match-game/index.js
@@ -1,68 +1,9 @@
-import Card from "./scripts/Card.js";
-import GameView from "./scripts/GameView.js";
-import Game from "./scripts/Game.js";
+import GameController from "./scripts/GameController.js";
 
-const GRID_SIZE = 4;
-const NUMBER_OF_CARD_TYPES = 2;
-const SEQUENCE_SIZE = 2;
-
-
-grid.addCardListener('click', processEvent);
-const deck = initCards(GRID_SIZE, NUMBER_OF_CARD_TYPES, SEQUENCE_SIZE);
-shuffle(deck);
-const game = new Game(deck);
-// const grid = document.querySelector('#grid');
-
-
-/**
- * Make cards.
- * @param {int} numberOfCards The total number of cards in the game
- * @param {int} numberOfTypes The number of different types of cards
- * @param {int} sequenceSize The number of cards of the same type which one must open in a row before they disappear
- */
-function initCards(numberOfCards, numberOfTypes, sequenceSize) {
-    // TODO numberOfCards must be even
-    // TODO numberOfPairs must be even
-    // TODO numberOfCards >= numberOfTypes * n
-
-    if (numberOfCards < numberOfTypes * sequenceSize) {
-        throw 'The number of cards can\'t be less than (number of card types * sequence size)';
-    }
-
-    if (numberOfCards % (numberOfTypes * sequenceSize)) {
-        throw 'The number of cards must by multiple of (number of card types * sequence size)';
-    }
-
-    const numberOfCardsOfEachType = (numberOfCards / numberOfTypes);
-    let cards = [];
-    let id = 0;
-    for (let type = 0; type < numberOfTypes; type++) {
-        for (let n = 0; n < numberOfCardsOfEachType; n++) {
-            cards.push(new Card(id, type, grid));
-            id += id;
-        }
-    }
-
-    // shuffle(cards);
-    cards.map((card, i) => card.id = i);
-    return cards;
-
+const gameSettings = {
+    cards: 36,
+    types: 6,
+    sequence: 2,
 }
 
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
-function processEvent(e) {
-    const cardIndex = parseInt(e.target.id, 10);
-    console.log('CLICKED', cardIndex)
-    if (isNaN(cardIndex)) {
-        return;
-    }
-    game.selectCard(cardIndex);
-    console.log(game.cards.map(c => `${c._state} ${c.type}`));
-    console.log('-----------------')
-}
+const game = new GameController(gameSettings);
