9a5a6228d238d63648ae831b5a18d0b8d32f6fe7
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-23 01:03:23 +0300

    Decouple CardsDeck from GameModel into a separate class

---
+++ match-match-game/scripts/CardsDeck.js
@@ -0,0 +1,58 @@
+import Card from "./Card.js";
+
+export default class CardDeck {
+    constructor(cards, types, sequence, view) {
+        this.cards = [];
+        this.initCards(cards, types, sequence, view);
+        this._shuffle();
+        this._refillCardIds();
+    }
+
+    initCards(numberOfCards, numberOfTypes, sequenceSize, view) {
+        if (numberOfCards % 2) {
+            throw 'The number of cards must be even';
+        }
+
+        if (numberOfCards < numberOfTypes * sequenceSize) {
+            throw 'The number of cards can\'t be less than (number of card types * sequence size)';
+        }
+
+        if (numberOfCards % (numberOfTypes * sequenceSize)) {
+            throw 'The number of cards must be multiple of (number of card types * sequence size)';
+        }
+
+        const numberOfCardsOfEachType = (numberOfCards / numberOfTypes);
+        let id = 0;
+        for (let type = 0; type < numberOfTypes; type++) {
+            for (let n = 0; n < numberOfCardsOfEachType; n++) {
+                this.cards.push(new Card(id, type, view));
+                id += 1;
+            }
+        }
+    }
+
+    getCardById(cardId) {
+        for (let card of this.cards) {
+            if (card.id === cardId) {
+                return card;
+            }
+        }
+        throw `there is no card with id: ${cardId} in the deck`
+    }
+
+    _shuffle() {
+        shuffleArray(this.cards);
+    }
+
+    _refillCardIds() {
+        this.cards.map((card, i) => card.id = i);
+    }
+
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
--- match-match-game/scripts/GameModel.js
+++ match-match-game/scripts/GameModel.js
@@ -1,67 +1,33 @@
 import OpenedCardsQueue from "./CardsQueue.js";
-import Card from "./Card.js";
+import CardsDeck from "./CardsDeck.js";
 
 export default class GameModel {
     constructor(settings, view) {
         this.cardsToPlay = settings.cards;
         this.openedCards = new OpenedCardsQueue(settings.sequence);
-        this.cards = this.initCards(
+        this.deck = new CardsDeck(
             settings.cards,
             settings.types,
-            settings.sequence, view);
-    }
-
-    initCards(numberOfCards, numberOfTypes, sequenceSize, view) {
-        // TODO numberOfCards >= numberOfTypes * n
-
-        if (numberOfCards % 2) {
-            throw 'The number of cards must be even';
-        }
-
-        if (numberOfCards < numberOfTypes * sequenceSize) {
-            throw 'The number of cards can\'t be less than (number of card types * sequence size)';
-        }
-
-        if (numberOfCards % (numberOfTypes * sequenceSize)) {
-            throw 'The number of cards must be multiple of (number of card types * sequence size)';
-        }
-
-        const numberOfCardsOfEachType = (numberOfCards / numberOfTypes);
-        let cards = [];
-        let id = 0;
-        for (let type = 0; type < numberOfTypes; type++) {
-            for (let n = 0; n < numberOfCardsOfEachType; n++) {
-                cards.push(new Card(id, type, view));
-                id += id;
-            }
-        }
-
-        shuffle(cards);
-        cards.map((card, i) => card.id = i);
-        return cards;
+            settings.sequence,
+            view);
     }
 
     isSelectableCard(card) {
         return (card.isClosed && !this.openedCards.blocked);
     }
 
-    getCardByIndex(cardIndex) {
-        return this.cards[cardIndex];
-    }
+    selectCard(cardId) {
+        const card = this.deck.getCardById(cardId);
 
-    selectCard(cardIndex) {
-        const card = this.getCardByIndex(cardIndex);
-
-        if (card.id !== cardIndex) {
+        if (card.id !== cardId) {
             throw 'wrong cardId';
         }
 
         if (!this.isSelectableCard(card)) {
-            console.log('you can\'t select this card!', card);
+            console.log('you can\'t select this card!', card); // debug mode
             return;
         }
 
-        // this.openedCards.process(card);
         if (this.openedCards.process(card)) {
             this.cardsToPlay -= this.openedCards.size;
             this.isGameOver();
@@ -74,12 +40,5 @@ export default class GameModel {
             console.log('You win!');
         }
         return result;
-    }
-}
-
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
     }
 }
