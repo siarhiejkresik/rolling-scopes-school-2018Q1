7cd16a228fdfc8b3208bce443e199748197344a8
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-22 23:40:53 +0300

    Move cards creation into GameModel

--- match-match-game/scripts/GameModel.js
+++ match-match-game/scripts/GameModel.js
@@ -1,10 +1,44 @@
 import OpenedCardsQueue from "./CardsQueue.js";
+import Card from "./Card.js";
 
 export default class GameModel {
-    constructor(cards, SEQUENCE_SIZE) {
-        this.cards = cards;
-        this.cardsToPlay = cards.length;
-        this.openedCards = new OpenedCardsQueue(SEQUENCE_SIZE);
+    constructor(settings, view) {
+        this.cardsToPlay = settings.cards;
+        this.openedCards = new OpenedCardsQueue(settings.sequence);
+        this.cards = this.initCards(
+            settings.cards,
+            settings.types,
+            settings.sequence, view);
+    }
+
+    initCards(numberOfCards, numberOfTypes, sequenceSize, view) {
+        // TODO numberOfCards >= numberOfTypes * n
+
+        if (numberOfCards % 2) {
+            throw 'The number of cards must be even';
+        }
+
+        if (numberOfCards < numberOfTypes * sequenceSize) {
+            throw 'The number of cards can\'t be less than (number of card types * sequence size)';
+        }
+
+        if (numberOfCards % (numberOfTypes * sequenceSize)) {
+            throw 'The number of cards must be multiple of (number of card types * sequence size)';
+        }
+
+        const numberOfCardsOfEachType = (numberOfCards / numberOfTypes);
+        let cards = [];
+        let id = 0;
+        for (let type = 0; type < numberOfTypes; type++) {
+            for (let n = 0; n < numberOfCardsOfEachType; n++) {
+                cards.push(new Card(id, type, view));
+                id += id;
+            }
+        }
+
+        shuffle(cards);
+        cards.map((card, i) => card.id = i);
+        return cards;
     }
 
     isSelectableCard(card) {
@@ -40,5 +74,12 @@ export default class GameModel {
             console.log('You win!');
         }
         return result;
+    }
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
