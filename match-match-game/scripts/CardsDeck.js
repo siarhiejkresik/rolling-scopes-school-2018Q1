ae26d08d84c176ab51c46adcb78230d3105d541e
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-23 16:21:43 +0300

    Add comments

--- match-match-game/scripts/CardsDeck.js
+++ match-match-game/scripts/CardsDeck.js
@@ -50,6 +50,10 @@ export default class CardDeck {
 
 }
 
+/**
+ * Randomize array element order in-place.
+ * Using Durstenfeld shuffle algorithm.
+ */
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
