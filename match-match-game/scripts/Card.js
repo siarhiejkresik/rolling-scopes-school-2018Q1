ca69b62c4a54f79485c8a39d70cf7635c93491e8
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-26 15:03:46 +0300

    Replace card states constant with one from constant.js

--- match-match-game/scripts/Card.js
+++ match-match-game/scripts/Card.js
@@ -1,42 +1,38 @@
 import Observable from "./Observable.js";
-
+import {CARD_STATE} from "./constants.js";
 export default class Card {
     constructor(id, type) {
         this.id = id;
         this.type = type;
-        this._state = STATE.CLOSED;
+        this._state = CARD_STATE.CLOSED;
 
         this.stateObservers = new Observable();
     }
 
     get isClosed() {
-        return this._state === STATE.CLOSED;
+        return this._state === CARD_STATE.CLOSED;
     }
 
     open() {
-        this._state = STATE.OPENED;
+        this._state = CARD_STATE.OPENED;
         this._stateChangeNotify();
     }
 
     close() {
-        this._state = STATE.CLOSED;
+        this._state = CARD_STATE.CLOSED;
         this._stateChangeNotify();
     }
 
     disable() {
-        this._state = STATE.DISABLED;
+        this._state = CARD_STATE.DISABLED;
         this._stateChangeNotify();
     }
 
     _stateChangeNotify() {
         // debug
         console.log(`CardModel: id ${this.id} is ${this._state} now`)
+        
         this.stateObservers.notify(this.id, this._state, this.type)
     }
 }
 
-export const STATE = {
-    OPENED: 'OPENED',
-    CLOSED: 'CLOSED',
-    DISABLED: 'DISABLED'
-}
