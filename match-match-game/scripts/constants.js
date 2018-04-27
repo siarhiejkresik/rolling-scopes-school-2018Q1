bf613806bf6c8d9d2ae0bca3182400e887d501ca
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-27 19:00:27 +0300

    Update constants

--- match-match-game/scripts/constants.js
+++ match-match-game/scripts/constants.js
@@ -6,6 +6,7 @@ const _VIEWS = 'welcome login menu game result';
 const _CONTROLS = 'play next login logout return';
 const _CARD_STATE = 'opened closed disabled';
 const _DIFFICULTIES = 'easy normal hard';
+const _PLAYER = 'firstname lastname email back difficulty';
 
 const VIEWS = EnumFromString(_VIEWS, '.');
 const CONTROLS = EnumFromString(_CONTROLS, '.');
@@ -13,14 +14,21 @@ const CARD_STATE = EnumFromString(_CARD_STATE);
 
 const DIFFICULTIES = EnumFromString(_DIFFICULTIES);
 const DIFFICULTIES_BUTTONS = EnumFromString(_DIFFICULTIES, '.')
+const PLAYER = EnumFromString(_PLAYER);
 
-
-
+const DB_KEY = 'matchmatch';
+const DEFAULT_DB = {
+    lastPlayerId: null,
+    players: []
+}
 
 export {
     VIEWS,
     CONTROLS,
     CARD_STATE,
     DIFFICULTIES,
-    DIFFICULTIES_BUTTONS
+    DIFFICULTIES_BUTTONS,
+    PLAYER,
+    DB_KEY,
+    DEFAULT_DB,
 };
