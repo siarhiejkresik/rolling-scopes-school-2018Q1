65e64343542d9c43114e4b7898e4641f5dd83e9e
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-04-29 14:58:16 +0300

    Format code

--- match-match-game/scripts/GameView.js
+++ match-match-game/scripts/GameView.js
@@ -1,74 +1,71 @@
-import {
-    CARD_STATE, THEMES
-} from "./constants.js";
+import { CARD_STATE, THEMES } from './constants.js';
+import { timeMSToMMSS } from './utils.js';
 
 export default class GameView {
-    constructor(size, cardBack=0) {
-        this.time = document.querySelector('.time');
-        this.grid = document.querySelector('.grid');
-        this.renderGrid(size, cardBack);
+  constructor(size, cardBack = 0) {
+    this.time = document.querySelector('.time');
+    this.grid = document.querySelector('.grid');
+    this.renderGrid(size, cardBack);
+  }
+
+  renderGrid(size, cardBack) {
+    // add cards to grid
+    for (let i = 0; i < size; i++) {
+      let card = cardTemplate(THEMES[cardBack]);
+      card = document.createRange().createContextualFragment(card);
+      this.grid.appendChild(card);
+      this.grid.lastChild.id = `card-${i}`;
     }
+  }
 
-    renderGrid(size, cardBack) {
-        // add cards to grid
-        for (let i = 0; i < size; i++) {
-            let card = cardTemplate(THEMES[cardBack]);
-            card = document.createRange().createContextualFragment(card);
-            this.grid.appendChild(card);
-            this.grid.lastChild.id = `card-${i}`;
-        }
+  updateCardView(id, state, type) {
+    // debug mode
+    console.log('GameView:  notify from GameModel:', id, state);
+
+    const selector = `#card-${id}`;
+    let card = this.grid.querySelector(selector);
+
+    if (state === CARD_STATE.OPENED) {
+      card.querySelector('.front').innerHTML = type;
+      this.openCard(card);
+    } else if (state === CARD_STATE.CLOSED) {
+      card.querySelector('.front').innerHTML = null;
+      this.closeCard(card);
+    } else if (state === CARD_STATE.DISABLED) {
+      this.disableCard(card);
+    } else {
+      throw new Error('unknown card state!');
     }
+  }
 
-    updateCardView(id, state, type) {
-        // debug mode
-        console.log('GameView:  notify from GameModel:', id, state);
+  updateTime(time) {
+    this.time.innerHTML = timeMSToMMSS(time);
+  }
 
-        const selector = `#card-${id}`;
-        let card = this.grid.querySelector(selector);
-        
-        if (state === CARD_STATE.OPENED) {
-            card.querySelector('.front').innerHTML = type;
-            this.openCard(card);
+  openCard(card) {
+    card.classList.add('opened');
+  }
 
-        } else if (state === CARD_STATE.CLOSED) {
-            card.querySelector('.front').innerHTML = null;
-            this.closeCard(card);
+  closeCard(card) {
+    card.classList.remove('opened');
+  }
 
-        } else if (state === CARD_STATE.DISABLED) {
-            this.disableCard(card);
+  disableCard(card) {
+    card.classList.add('disabled');
+  }
 
-        } else {
-            throw new('unknown card state!')
-        }
-    }
-
-    updateTime(time) {
-        this.time.innerHTML = timeMSToMMSS(time);
-    }
-
-    openCard(card) {
-        card.classList.add('opened');
-    }
-
-    closeCard(card) {
-        card.classList.remove('opened');
-    }
-
-    disableCard(card) {
-        card.classList.add('disabled');
-    }
-
-
-    cleanUp() {
-        this.grid.innerHTML = null; // removes all cards
-        this.grid.outerHTML = this.grid.outerHTML; // removes all listeners
-    }
+  cleanUp() {
+    this.grid.innerHTML = null; // removes all cards
+    this.grid.outerHTML = this.grid.outerHTML; // removes all listeners
+  }
 }
 
-const cardTemplate = (cardBack) => {return `
+const cardTemplate = cardBack => {
+  return `
 <div class="card">
     <div class="content">
         <div class="face back ${cardBack}"></div>
         <div class="face front"></div>
     </div>
-</div>`;}
+</div>`;
+};
