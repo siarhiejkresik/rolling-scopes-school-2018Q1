e6beffbd7d140bd6317ddd34cffd581223f7e9ad
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-07-20 18:49:17 +0300

    Add loaders to webpack config

--- match-match-game-react/webpack.config.js
+++ match-match-game-react/webpack.config.js
@@ -14,6 +14,22 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.woff2?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   devtool: '#eval-source-map',
