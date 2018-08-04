a1cc9a48fde2801a955d1bde0d549a623efd2f2d
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-04 21:01:56 +0300

    Add actions

---
+++ match-match-game-react/src/actions/index.js
@@ -0,0 +1,60 @@
+export const CARD_STATES = {
+  OPENED: 'OPENED',
+  CLOSED: 'CLOSED',
+  DISABLED: 'DISABLED',
+};
+
+export const SET_CARD_STATE = 'SET_CARD_STATE';
+
+export const ADD_CARD_TO_QUEUE = 'ADD_CARD_TO_QUEUE';
+export const CLEAR_QUEUE = 'CLEAR_QUEUE';
+export const BLOCK_QUEUE = 'BLOCK_QUEUE';
+export const UNBLOCK_QUEUE = 'UNBLOCK_QUEUE';
+export const SET_MAX_QUEUE_SIZE = 'SET_MAX_QUEUE_SIZE';
+export const CLOSE_CARDS = 'CLOSE_CARDS';
+
+export const SET_DIFFICULTY = 'SET_DIFFICULTY';
+export const SET_CARD_BACK = 'SET_CARD_BACK';
+export const GENERATE_CARD_DECK = 'GENERATE_CARD_DECK';
+
+export const setCardState = ({ id, state }) => ({
+  type: SET_CARD_STATE,
+  id,
+  state,
+});
+
+export const addCardToQueue = ({ id }) => ({
+  type: ADD_CARD_TO_QUEUE,
+  id,
+});
+
+export const clearQueue = () => ({
+  type: CLEAR_QUEUE,
+});
+
+export const blockQueue = () => ({
+  type: BLOCK_QUEUE,
+});
+
+export const unBlockQueue = () => ({
+  type: UNBLOCK_QUEUE,
+});
+
+export const setMaxQueueSize = maxSize => ({ type: SET_MAX_QUEUE_SIZE, maxSize });
+
+export const closeCards = () => ({ type: CLOSE_CARDS });
+
+export const setDifficulty = difficulty => ({
+  type: SET_DIFFICULTY,
+  difficulty,
+});
+
+export const setCardBack = cardBack => ({
+  type: SET_CARD_BACK,
+  cardBack,
+});
+
+export const generateCardDeck = difficulty => ({
+  type: GENERATE_CARD_DECK,
+  difficulty,
+});
