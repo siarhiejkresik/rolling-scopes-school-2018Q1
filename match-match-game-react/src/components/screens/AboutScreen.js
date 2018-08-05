609370a652d1bb8c62b383a2db9c0272a2b59de9
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-05 15:02:39 +0300

    Move string literals to constants

--- match-match-game-react/src/components/screens/AboutScreen.js
+++ match-match-game-react/src/components/screens/AboutScreen.js
@@ -3,16 +3,25 @@ import { Link } from 'react-router-dom';
 
 import Heading from '../presentational/Heading';
 
+const GAME_DESCRIPTION = `Concentration, also known as Match Match, Match Up, Memory, Pelmanism, Shinkei-suijaku,
+Pexeso or simply Pairs, is a card game in which all of the cards are laid face down on a
+surface and two cards are flipped face up over each turn. The object of the game is to turn
+over pairs of matching cards.`;
+
+const GAME_RULES = [
+  'Turn over any two cards',
+  'If the two cards match, they disappear',
+  "If they don't match, they flip back over",
+  'The game is over when all the cards have been disappeared',
+];
+
 const AboutScreen = () => (
   <section className="welcome">
     <Heading title="Welcome!" level={2} />
     <article>
       <Heading title="About the game" level={3} />
       <blockquote>
-        {`Concentration, also known as Match Match, Match Up, Memory, Pelmanism, Shinkei-suijaku,
-        Pexeso or simply Pairs, is a card game in which all of the cards are laid face down on a
-        surface and two cards are flipped face up over each turn. The object of the game is to turn
-        over pairs of matching cards.`}
+        {GAME_DESCRIPTION}
         <cite>
           <a href="https://en.wikipedia.org/wiki/Concentration_(game)">
             {'wikipedia.org'}
@@ -21,22 +30,14 @@ const AboutScreen = () => (
       </blockquote>
       <Heading title="How to play" level={3} />
       <ul>
-        <li>
-          {'Turn over any two cards'}
-        </li>
-        <li>
-          {'If the two cards match, they disappear'}
-        </li>
-        <li>
-          {"If they don't match, they flip back over"}
-        </li>
-        <li>
-          {'The game is over when all the cards have been disappeared'}
-        </li>
+        {GAME_RULES.map((line, index) => (
+          <li key={index}>
+            {line}
+          </li>
+        ))}
       </ul>
     </article>
-    {/* <Link to="/login"> */}
-    <Link to="/menu">
+    <Link to="/login">
       <button className="next" type="button">
         {'Next'}
       </button>
