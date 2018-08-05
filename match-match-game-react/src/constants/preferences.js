3776891ec26309ab57293b54e28606bcd734f963
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-05 15:04:22 +0300

    Add KIDS diffuculty for easier testing

--- match-match-game-react/src/constants/preferences.js
+++ match-match-game-react/src/constants/preferences.js
@@ -1,10 +1,16 @@
 export const DIFFICULTY = {
+  KIDS: 'KIDS',
   EASY: 'EASY',
   NORMAL: 'NORMAL',
   HARD: 'HARD',
 };
 
 export const PREFERENCES = {
+  [DIFFICULTY.KIDS]: {
+    cards: 4,
+    types: 2,
+    sequence: 2,
+  },
   [DIFFICULTY.EASY]: {
     cards: 16,
     types: 8,
