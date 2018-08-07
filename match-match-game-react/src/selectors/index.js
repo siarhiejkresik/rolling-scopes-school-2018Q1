09dc4bc6e2b8bf8f86690281ccd3dc31f2185587
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-07 13:52:36 +0300

    Get the number of cards to match from game preferences

--- match-match-game-react/src/selectors/index.js
+++ match-match-game-react/src/selectors/index.js
@@ -1,11 +1,12 @@
 import { createSelector } from 'reselect';
 
 import CARD_STATES from '../constants/card-states';
+import { PREFERENCES } from '../constants/preferences';
 
 const getCards = state => state.cards;
 const getQueue = state => state.queue;
+const queueMaxSize = state => PREFERENCES[state.preferences.difficulty].sequence;
 
-const queueMaxSize = state => state.queue.maxSize;
 export const queueCurrSize = createSelector([getQueue], queue => queue.cardIds.length);
 
 export const isQueueFull = createSelector([queueMaxSize, queueCurrSize], (maxSize, currSize) => {
