63d704ae64ec77f4f1cc4e43b027042ffbeb7370
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-08 16:32:40 +0300

    Update the scores reducer to clear scores

--- match-match-game-react/src/reducers/scores.js
+++ match-match-game-react/src/reducers/scores.js
@@ -1,11 +1,13 @@
 import sortBy from 'lodash.sortby';
 
-import { RECEIVE_SCORES } from '../actions';
+import { RECEIVE_SCORES, CLEAR_SCORES } from '../actions';
 
 export default (state = [], action) => {
   switch (action.type) {
     case RECEIVE_SCORES:
       return sortBy(action.scores, ['score', 'updatedAt', 'username']);
+    case CLEAR_SCORES:
+      return [];
     default:
       return state;
   }
