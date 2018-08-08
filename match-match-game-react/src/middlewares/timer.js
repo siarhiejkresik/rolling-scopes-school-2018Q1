ee04a39d9749cad300994000018471b86aa56ebb
Siarhiej Kresik <siarhiej.kresik@gmail.com>
2018-08-08 17:56:58 +0300

    Replace string literal for timer tick action with constant

--- match-match-game-react/src/middlewares/timer.js
+++ match-match-game-react/src/middlewares/timer.js
@@ -1,4 +1,4 @@
-import { START_TIMER, TICK_TIMER, STOP_TIMER } from '../actions';
+import { START_TIMER, STOP_TIMER, tickTimer } from '../actions';
 
 const TICK_INTERVAL = 1000;
 
@@ -6,7 +6,7 @@ let interval;
 
 export default store => next => (action) => {
   if (action.type === START_TIMER) {
-    interval = setInterval(() => store.dispatch({ type: TICK_TIMER }), TICK_INTERVAL);
+    interval = setInterval(() => store.dispatch(tickTimer()), TICK_INTERVAL);
   } else if (action.type === STOP_TIMER) {
     clearInterval(interval);
   }
